8b536db421c7072d58afcbc854a813f6042c2ef2
angec <angec@angle>
2026-03-25 11:48:58 +0100

    First commit angele

---
+++ src/main/java/tpjava/projetjava/Puzzle.java
@@ -0,0 +1,22 @@
+
+package tpjava.projetjava;
+
+public abstract class Puzzle {
+    protected String imagePath;
+    protected String prompt;
+
+    public Puzzle(String imagePath, String prompt) {
+        this.imagePath = imagePath;
+        this.prompt = prompt;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public String getPrompt() {
+        return prompt;
+    }
+
+    public abstract String getAnswer();
+}
