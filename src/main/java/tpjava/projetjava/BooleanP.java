c089a2fb0d5b3c633e5e24eb5be8c7ce4fe72894
Maiwe <Maiwe@10.187.7.174>
2026-04-01 15:02:38 +0200

    JPanel BooleanP

--- src/main/java/tpjava/projetjava/BooleanP.java
+++ src/main/java/tpjava/projetjava/BooleanP.java
@@ -13,16 +13,46 @@ import javax.swing.*;
  */
 public class BooleanP extends Puzzle{
     public JPanel AnswerPanel;
+    
+    private JLabel label;
+    private JRadioButton vrai;
+    private JRadioButton faux;
+    private ButtonGroup group;
 
-    public Boolean(String imagePath, String prompt) {
+    public BooleanP(String imagePath, String prompt) {
         super(imagePath, prompt);
+        setup();
     }
 
+    private void setup() {
+        // 1. Initialisation du AnswerPanel avec son layout
+        AnswerPanel = new JPanel();
+        AnswerPanel.setLayout(new FlowLayout(FlowLayout.LEADING));
+
+        // 2. Initialisation des composants
+        group = new ButtonGroup();
+        label = new JLabel("Réponse : ");
+        vrai = new JRadioButton("Vrai");
+        faux = new JRadioButton("Faux");
+
+        // 3. Organisation logique (exclusivité des boutons)
+        group.add(vrai);
+        group.add(faux);
+
+        // 4. Ajout des composants au AnswerPanel
+        AnswerPanel.add(label);
+        AnswerPanel.add(vrai);
+        AnswerPanel.add(faux);
+        
+    }
+    
     @Override
     public String getAnswer() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        String Answer;
+        Answer="null";
+        if (vrai.isSelected()) Answer= "true";
+        if (faux.isSelected()) Answer= "false";
+        return routes.get(Answer);
     }
-    
-    
     
 }
