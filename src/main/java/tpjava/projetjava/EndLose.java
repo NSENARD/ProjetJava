3a48f0c31ca1be81ec994009a4f4c31553c6f928
angec <angec@angle>
2026-04-22 10:58:50 +0200

    creation des classes end win et end lose

---
+++ src/main/java/tpjava/projetjava/EndLose.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tpjava.projetjava;
+
+import java.util.HashMap;
+
+public class EndLose extends Puzzle {
+    
+    public EndLose(String image, String prompt, HashMap<String, String> routes) {
+        super(image, prompt, routes);
+    }
+
+    public void afficher() {
+        System.out.println("Défaite...");
+        System.out.println(prompt);
+    }
+
+    @Override
+    public String getAnswer() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+}
+
+
